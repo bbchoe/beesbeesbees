cf9912717812dfa01ddc4857b892e5976cd90314
Brian Choe <brian@visibook.com>
2017-10-16 12:29:47 -0700

    finished beesbeesbees exercise

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,39 @@
-var Bee = function() {
+
+var Grub = function() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
 
+Grub.prototype.eat = function() {};
+
+// Create Bee subclass from Grub superclass
+var Bee = function() {
+  Grub.call(this)
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+};
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+
+//Create HoneyMakerBee subclass from Bee superclass
+var HoneyMakerBee = function() {
+  debugger;
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeypot = 0;
+}
+
+HoneyMakerBee.prototype = Object(Bee.prototype);
+HoneyMakerBee.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeypot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeypot--;
+}
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
+//Create HoneyMakerBee subclass from Bee superclass
 var HoneyMakerBee = function() {
-};
+  debugger;
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+}
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+}
